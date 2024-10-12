726ca1f7c26eef52dd93b5770d31150ad14d852f
ViniciusVieira-web <viniciusvieiramorim76@gmail.com>
2024-10-12 12:07:30 -0300

    fim do curso

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let n = [5, 8, 3]
+n.push(1)
+n.sort()
+
+console.log(`O vetor tem ${n.length} posições`)
+console.log(`O primeiro valor do vetor é ${n}`)
+let pos = n.indexOf(8)
+conseole.log(`O valor 8 está na posição ${pos}`)
---
+++ aula15/vetor.js
@@ -0,0 +1,12 @@
+let valores = [2,4,6,3]
+valores.sort()
+
+for (let pos in valores){
+    console.log (`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
+
+
+// for (let pos = 0; pos < valores.length; pos++){
+//     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+// }
